2ce8130768c821d5edb317350ad241d2ef286e3d
Vernon Hudson <vhudson@crimson.ua.edu>
2023-12-13 11:55:32 -0600

    Used switch statement to allow user iput to generate expression based on input

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?\nChoose between the following:\nMath\nScience\nHistory\nEnglish|Gym");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case"math":
+                    Console.WriteLine("I Hate Math");
+                    break;
+                case "science":
+                    Console.WriteLine("Still Not Math");
+                    break;
+                case "english":
+                    Console.WriteLine("ewwwwwww");
+                    break;
+                case "history":
+                    Console.WriteLine("Why not math?");
+                    break;
+                case "gym":
+                    Console.WriteLine("MEATHEAD!!!!");
+                    break;
+                default:
+                    Console.WriteLine("Should have chosen math");
+                    break;
+            }
         }
     }
 }
